3e8399bdf5bb834671da033d2e687c93eff435ca
Rei <reidumand@outlook.com>
2022-11-12 11:31:26 +0900

    Feat: Set up canvas component for test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
 
+import Canvas from './components/Canvas'
+
 function App() {
   return (
     <div className="App">
-     
+      <Canvas />
     </div>
   );
 }
---
+++ client/src/components/Canvas.tsx
@@ -0,0 +1,12 @@
+import './Canvas.css';
+import React from 'react';
+
+function Canvas() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default Canvas;
