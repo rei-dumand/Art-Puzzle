2bc77ff3034b43b40fec3827f37a240d6b38f370
Rei <reidumand@outlook.com>
2022-11-13 09:02:42 +0900

    Feat: Add Tile class

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,30 +1,70 @@
 import './Canvas.css';
 import React, { useEffect, useRef, useState } from 'react';
+import imgURL from '../img/1964.336 - Paris Street; Rainy Day.jpg';
 
 function Canvas() {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
     const [canvasHasMounted, SetCanvasHasMounted] = useState<boolean | null>(false);
 
+    let frameCount = 0;
+    let animationFrameID: number;
+
+    let cols = 10;
+    let row = 10;
+    let w, h;
+
+
+    let image = new Image();
+    image.src = imgURL;
+
+    const draw = (ctx : CanvasRenderingContext2D, frameCount : number) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        
+        ctx.fillStyle = '#739173'
+        ctx.fillRect(0, 0, ctx!.canvas.width, ctx!.canvas.height)
+
+        ctx.fillStyle = '#000000'
+        ctx.beginPath()
+        // ctx.arc(100, 100, 20, 0, 2*Math.PI)
+        ctx.arc(150, 150, 20*Math.sin(frameCount*0.03)**2, 0, 2*Math.PI)
+        ctx.fill()
+    }
+
     useEffect(() => {
         if (canvasHasMounted) {
             console.log(canvasRef)
             const canvas : HTMLCanvasElement | null = canvasRef.current;
-            if (!canvas) { }
-            else {
+            if (canvas) {
                 const context = canvas.getContext('2d');
-                context!.fillStyle = '#000000'
-                context!.fillRect(0, 0, context!.canvas.width, context!.canvas.height)
+                if (context) {
+                    const render = () => {
+                        frameCount++
+                        draw(context, frameCount)
+                        animationFrameID = window.requestAnimationFrame(render)
+                    }
+                    render()
+
+                    return () => {
+                        window.cancelAnimationFrame(animationFrameID)
+                    }
+                }
             }
         } else {
             SetCanvasHasMounted(true)  
         }
-    }, [canvasHasMounted])
+
+
+    }, [canvasHasMounted, draw])
 
 
     return (
         <>
-            <canvas ref={canvasRef}></canvas>
+            <canvas ref={canvasRef} height={400} width={400}></canvas>
+            <div draggable="true">
+            <img src={imgURL}></img>
+
+            </div>
         </>
     );
 }
