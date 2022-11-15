2723a6cf17b860155242e085bdd9f4baf51abee3
Rei <reidumand@outlook.com>
2022-11-15 21:47:10 +0900

    Feat: Add server-client connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css';
-import React, {useRef, useEffect, useState} from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import axios from 'axios'
+import api from './services/axiosconfig'
 
 import Home from './pages/Home'
 import Profile from './pages/Profile'
@@ -14,66 +15,71 @@ import { Artwork } from './types';
 
 function App() {
 
-  let artworkData = useRef<Artwork[] | null>(null);
+    let artworkData = useRef<Artwork[] | null>(null);
 
-  let fetchedOnce = useRef<boolean>(false);
+    let fetchedOnce = useRef<boolean>(false);
 
-  const [arrImgID, setArrImgID] = useState<string[] | null>(null);
+    const [arrImgID, setArrImgID] = useState<string[] | null>(null);
 
-  async function fetchArtworkMetadata() {
-      artworkData.current = await axios
-          .post<{data : Artwork[]}>('https://api.artic.edu/api/v1/artworks/search', {
-              "fields": ["id", "title", "artist_title", "date_start", "date_end", "place_of_origin", "dimensions", "medium_display", "image_id"],
-              "limit": 100,
-              "query": {
-                  "bool": {
-                      "must": [
-                          { "match": { "classification_titles": { "query": "painting" } } },
-                          { "match": { "style_titles": { "query": "impressionism" } } },
-                      ]
-                  }
-              }
-          })
-          .then(function (response) {
-              return response.data.data
-          })
-          .catch(function (error) {
-              console.error(error);
-              return null
-          });
-  }
+    async function fetchArtworkMetadata() {
+        artworkData.current = await axios
+            .post<{ data: Artwork[] }>('https://api.artic.edu/api/v1/artworks/search', {
+                "fields": ["id", "title", "artist_title", "date_start", "date_end", "place_of_origin", "dimensions", "medium_display", "image_id"],
+                "limit": 100,
+                "query": {
+                    "bool": {
+                        "must": [
+                            { "match": { "classification_titles": { "query": "painting" } } },
+                            { "match": { "style_titles": { "query": "impressionism" } } },
+                        ]
+                    }
+                }
+            })
+            .then(function (response) {
+                return response.data.data
+            })
+            .catch(function (error) {
+                console.error(error);
+                return null
+            });
+    }
 
-  // Call all artworks of relevant collection
-  useEffect(() => {
-      async function setupImgGrid() {
-          await fetchArtworkMetadata();
-          let arrArtworkData: Artwork[] = artworkData.current!;
-          // console.log(arrArtworkData)
-          setArrImgID(arrArtworkData.map((item) => item.image_id))
-      }
+    // Call all artworks of relevant collection
+    useEffect(() => {
+        async function setupImgGrid() {
+            await fetchArtworkMetadata();
+            let arrArtworkData: Artwork[] = artworkData.current!;
+            // console.log(arrArtworkData)
+            setArrImgID(arrArtworkData.map((item) => item.image_id))
+        }
 
-      if (!fetchedOnce.current) setupImgGrid();
+        if (!fetchedOnce.current) setupImgGrid();
 
-      fetchedOnce.current = true;
+        fetchedOnce.current = true;
 
-  }, [fetchedOnce])
+    }, [fetchedOnce])
 
-  // useEffect(()=> {
-  //   console.log(artworkData)
-  // }, [artworkData])
+    useEffect(() => {
+        api
+            .get('/example/')
+            .then((res) => {
+                console.log(res)
+            })
+        console.log(artworkData)
+    }, [artworkData])
 
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home artworkData={artworkData.current} arrImgID={arrImgID}/>} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/explore" element={<Explore /*artworkData={artworkData}*//>} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/home" element={<Home artworkData={artworkData.current} arrImgID={arrImgID} />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/explore" element={<Explore /*artworkData={artworkData}*/ />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
