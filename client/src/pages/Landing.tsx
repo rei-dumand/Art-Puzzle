3b89d36aaf05305fcc5e9550f483572d8b50cc1e
Rei <reidumand@outlook.com>
2023-02-07 10:36:02 +0000

    Feat: Add Spline support

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Landing.css'
+import Spline from '@splinetool/react-spline';
 
 function Landing(props: any) {
 
     return (
         <main id='landing'>
-            	
-                <section id="landing-catchphrase">
-                    Your Online Fine Arts Puzzles
-                </section>
 
-                <h1 id='landing-page__title'>
-                    Ozzler
+            {/* <Spline scene="https://prod.spline.design/KBu3HABo4DflGhrU/scene.splinecode" /> */}
+           {/* <Spline scene="https://prod.spline.design/bfz8SbA-9d49pjTm/scene.splinecode" /> */}
+
+
+            <section id="spinner"></section>
+
+            {/* <section id="landing-catchphrase">
+                    Your Online Fine Arts Puzzles
+                </section> */}
+
+            {/* <h1 id='landing-page__title'>
+                    Eazzle
                 </h1>
 
                 <section id='landing-action'>
@@ -20,7 +27,7 @@ function Landing(props: any) {
                     |
                     <Link to="signup"><button className='landing-action__button'>signup</button></Link>
 
-                </section>
+                </section> */}
         </main>
 
     )
